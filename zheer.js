54a0c043fe73f36a7c3c169739ada20003e1cad0
drzheercid <76868255+drzheercid@users.noreply.github.com>
2021-03-23 19:44:45 +0300

    Update zheer.js

--- zheer.js
+++ zheer.js
@@ -1,1 +1,22 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log();
+  let statuses = [``, ``]; /// لێرە بیۆ دانی بۆ ستریمنگ
+  setInterval(function() {
+    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
+    client.user.setActivity(STREAMING, {
+      type: "STREAMING",
+      url: "https://www.twitch.tv/zheer"
+    });
+  }, 2000);
+});
+
+
+client.login("TOKEN"); //// لێرە تۆکینی بۆت یان ئەکاونت دادەنێت
+
+
+
+
 
